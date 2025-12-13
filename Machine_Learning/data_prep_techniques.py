cbe649cb04cd197d4b000a2295cb3e3fe6817c67
Craig Murray <cmurray4492@gmail.com>
2025-12-13 13:26:19 -0600

    Dec 13 2025

---
+++ Machine_Learning/data_prep_techniques.py
@@ -0,0 +1,35 @@
+# Various data prep techniques
+
+# dataframe name customers
+import pandas as pd
+
+cust = pd.DataFrame({'Income': ['$1,200', '$2,500', '$3,750']})
+
+cust['Income'] = cust.Income.str.replace('$', '').str.repalce(',', '')
+cust['Income'] = pd.to_numeric(cust['Income'])
+cust.info()  # to verify changes
+
+# change floats to ints - more memory efficient
+cust['Income'] = cust['Income'].astype('int')
+cust.info()  # to verify changes
+
+# change object to datetime
+cust = pd.DataFrame({'DOB': ['01-15-1990', '06-30-1985', '12-05-2000']})
+cust['DOB'] = pd.to_datetime(cust['DOB'], format='%m/-%d/-%y')
+cust.info()  # to verify changes
+
+# Extract date time elements
+cust['Year'] = cust['DOB'].dt.year
+cust['Month'] = cust['DOB'].dt.month
+cust['Day'] = cust['DOB'].dt.day
+cust['DayofWeek'] = cust['DOB'].dt.dayofweek
+
+# Drop DOB column
+cust = cust.drop(columns=['DOB'])
+cust.info()  # to verify changes
+
+# need bin date time elements
+bins = [0, 1980, 1990, 2000, 2010]
+labels = ['Before 80s', '80s', '90s', '2000s']
+cust['DOB_Binned'] = pd.cut(cust['Year'], bins=bins, labels=labels)
+cust.info()  # to verify changes
